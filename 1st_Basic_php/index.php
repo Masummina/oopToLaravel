d7439e7009edd059cfa4c1f47507cb6beaa5b855
Masum <masum2mina@gmai.com>
2020-10-25 23:48:35 +0600

    Laravel first class

---
+++ 1st_Basic_php/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+// we learn string
+// string is a sequence of characters like hello word;
+// now start
+
+	echo "Hello word!";// this is a string
+	echo $br = "<br>";
+	echo strlen("Hello Word");
+	echo $br;
+	echo str_word_count("Hello Word!, I love Bangladesh ");
+	echo $br;
+	echo strrev("Hello word");
+	echo $br;
+	echo str_replace("Hello", "My", "Hello Word");
+	echo $br;
+	echo str_repeat("hello", "3");
+
+ ?>
